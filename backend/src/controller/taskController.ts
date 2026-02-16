e344bc93be264b74634d14707c4d973c142b898d
Apurvborhade <apurvaborhadee@gmail.com>
2026-02-16 23:21:34 +0530

    Redis Cache

--- backend/src/controller/taskController.ts
+++ backend/src/controller/taskController.ts
@@ -3,7 +3,7 @@ import { Request, Response } from "express";
 
 import { AuthRequest } from "../middleware/auth.middleware.js";
 import { prisma } from "../utils/prisma.js";
-
+import { redis } from "../utils/redis.js";
 
 export const createTask = async (req: AuthRequest, res: Response) => {
   const { title } = req.body;
@@ -14,12 +14,25 @@ export const createTask = async (req: AuthRequest, res: Response) => {
       userId: req.user!.id,
     },
   });
+  await redis.del(`tasks:user:${req.user!.id}`);
+  await redis.del("tasks:admin");
 
   res.status(201).json(task);
 };
 
 
 export const getTasks = async (req: AuthRequest, res: Response) => {
+  const cacheKey =
+    req.user!.role === "ADMIN"
+      ? "tasks:admin"
+      : `tasks:user:${req.user!.id}`;
+
+  const cached = await redis.get(cacheKey);
+
+  if (cached) {
+    return res.json(JSON.parse(cached));
+  }
+
   let tasks;
 
   if (req.user!.role === "ADMIN") {
@@ -31,6 +44,8 @@ export const getTasks = async (req: AuthRequest, res: Response) => {
       where: { userId: req.user!.id },
     });
   }
+  // Store in cache (expire in 60 seconds)
+  await redis.set(cacheKey, JSON.stringify(tasks), "EX", 60);
 
   res.json(tasks);
 };
@@ -59,6 +74,8 @@ export const updateTask = async (req: AuthRequest, res: Response) => {
     data: { title, completed },
   });
 
+  await redis.del(`tasks:user:${task.userId}`);
+  await redis.del("tasks:admin")
   res.json(updatedTask);
 };
 
